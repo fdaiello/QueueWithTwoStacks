b6034ef591db327338636eae0577b5cc4176a476
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-02-08 14:50:54 -0300

    comments

--- QueueWithTwoStacks/Program.cs
+++ QueueWithTwoStacks/Program.cs
@@ -5,7 +5,153 @@ namespace QueueWithTwoStacks
 {
     class Solution
     {
+        /*
+         * This is the BEST Solution
+         * Allways insert at Stack1
+         * Allways remove and peek from Stack2
+         * If Stack2 is empty, flip all values from Stack1 to Stack2 ( Pop and Push )
+         */
+        class MyQueue<T>
+        {
+            // Two stacks that simulate a Queue:
+            readonly Stack<T> stack1;
+            readonly Stack<T> stack2;
 
+            public MyQueue()
+            {
+                stack1 = new Stack<T>();
+                stack2 = new Stack<T>();
+            }
+
+            public void Enqueue(T x)
+            {
+                stack1.Push(x);
+            }
+
+            public void Dequeue()
+            {
+
+                if (stack2.Count == 0)
+                {
+                    while (stack1.Count > 0)
+                    {
+                        stack2.Push(stack1.Pop());
+                    }
+                }
+
+                stack2.Pop();
+            }
+            public T Peek()
+            {
+                if (stack2.Count == 0)
+                {
+                    while (stack1.Count > 0)
+                    {
+                        stack2.Push(stack1.Pop());
+                    }
+                }
+
+                return stack2.Peek();
+            }
+        }
+
+        /*
+         * This Alternative flips all stacks all times. Works, but its slow
+         */
+        class MyQueue2
+        {
+            private readonly Stack<int> stack1 = new Stack<int>();
+            private readonly Stack<int> stack2 = new Stack<int>();
+            private Commands LastCommand = Commands.none;
+
+            private enum Commands
+            {
+                none,
+                enqueue,
+                dequeue,
+                peek
+            }
+
+            internal void Enqueue(int value)
+            {
+
+                if (LastCommand != Commands.enqueue)
+                {
+                    FlipStacks();
+                }
+
+                if (stack2.Count == 0)
+                {
+                    stack1.Push(value);
+                }
+                else
+                {
+                    stack2.Push(value);
+                }
+
+                LastCommand = Commands.enqueue;
+            }
+            internal int Dequeue()
+            {
+                if (LastCommand == Commands.enqueue)
+                {
+                    FlipStacks();
+                }
+
+                LastCommand = Commands.dequeue;
+
+                if (stack1.Count > 0)
+                {
+                    return stack1.Pop();
+                }
+                else
+                {
+                    return stack2.Pop();
+                }
+            }
+            internal int Peek()
+            {
+                if (LastCommand == Commands.enqueue)
+                {
+                    FlipStacks();
+                }
+
+                LastCommand = Commands.peek;
+
+                if (stack1.Count > 0)
+                {
+                    return stack1.Peek();
+                }
+                else
+                {
+                    return stack2.Peek();
+                }
+
+            }
+            private void FlipStacks()
+            {
+                if (stack1.Count == 0)
+                {
+                    while (stack2.Count > 0)
+                    {
+                        stack1.Push(stack2.Pop());
+                    }
+                }
+                else
+                {
+                    while (stack1.Count > 0)
+                    {
+                        stack2.Push(stack1.Pop());
+                    }
+                }
+            }
+
+        }
+
+        /*
+         * This Alternative flips all stacks all times. Works, but its slow
+         *  Almost the same as the above version ... I tryed to gain some performace by caching peek Value, but it did't worked much
+         */
         class MyQueue1
         {
             private readonly Stack<int> stack1 = new Stack<int>();
@@ -97,96 +243,10 @@ namespace QueueWithTwoStacks
             }
 
         }
-        class MyQueue2
-        {
-            private readonly Stack<int> stack1 = new Stack<int>();
-            private readonly Stack<int> stack2 = new Stack<int>();
-            private Commands LastCommand = Commands.none;
 
-            private enum Commands
-            {
-                none,
-                enqueue,
-                dequeue,
-                peek
-            }
-
-            internal void Enqueue(int value)
-            {
-
-                if ( LastCommand != Commands.enqueue)
-                {
-                    FlipStacks();
-                }
-
-                if (stack2.Count == 0)
-                {
-                    stack1.Push(value);
-                }
-                else
-                {
-                    stack2.Push(value);
-                }
-
-                LastCommand = Commands.enqueue;
-            }
-            internal int Dequeue()
-            {
-                if (LastCommand == Commands.enqueue)
-                {
-                    FlipStacks();
-                }
-
-                LastCommand = Commands.dequeue;
-
-                if (stack1.Count > 0)
-                {
-                    return stack1.Pop();
-                }
-                else
-                {
-                    return stack2.Pop();
-                }
-            }
-            internal int Peek()
-            {
-                if (LastCommand == Commands.enqueue)
-                {
-                    FlipStacks();
-                }
-
-                LastCommand = Commands.peek;
-
-                if (stack1.Count > 0)
-                {
-                    return stack1.Peek();
-                }
-                else
-                {
-                    return stack2.Peek();
-                }
-
-            }
-            private void FlipStacks()
-            {
-                if (stack1.Count == 0)
-                {
-                    while (stack2.Count > 0)
-                    {
-                        stack1.Push(stack2.Pop());
-                    }
-                }
-                else
-                {
-                    while (stack1.Count > 0)
-                    {
-                        stack2.Push(stack1.Pop());
-                    }
-                }
-            }
-
-        }
-
+        /*
+         * This dont use stacks. It uses the Queue structure from the framewor. I used it for testing
+         */
         class MyQueue3
         {
             private Queue<int> queue = new Queue<int>();
@@ -205,6 +265,9 @@ namespace QueueWithTwoStacks
             }
         }
 
+        /*
+         * Here another idea, but it seems to have some bug
+         */
         class MyQueue4<T>
         {
             Stack<T> stack1 = new Stack<T>();
@@ -239,49 +302,6 @@ namespace QueueWithTwoStacks
             }
         }
 
-        class MyQueue<T>
-        {
-            // Two stacks that simulate a Queue:
-            readonly Stack<T> stack1;
-            readonly Stack<T> stack2;
-
-            public MyQueue()
-            {
-                stack1 = new Stack<T>();
-                stack2 = new Stack<T>();
-            }
-
-            public void Enqueue(T x)
-            {
-                stack1.Push(x);
-            }
-
-            public void Dequeue()
-            {
-
-                if (stack2.Count==0)
-                {
-                    while (stack1.Count>0)
-                    {
-                        stack2.Push(stack1.Pop());
-                    }
-                }
-
-                stack2.Pop();
-            }
-            public T Peek()
-            {
-                if (stack2.Count==0)
-                {
-                    while (stack1.Count>0)
-                    {
-                        stack2.Push(stack1.Pop());
-                    }
-                }
-
-                return stack2.Peek();
-            }
-        }
 
         static void Main(string[] args)
         {
